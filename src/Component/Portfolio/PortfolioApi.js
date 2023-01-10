e9ff4c7eb3cb84da09fb61a1c975a4c5cf3968b7
Ankush8950 <69650280+Ankush8950@users.noreply.github.com>
2023-01-10 22:55:44 +0530

    add product in portfolio

--- src/Component/Portfolio/PortfolioApi.js
+++ src/Component/Portfolio/PortfolioApi.js
@@ -1,44 +1,44 @@
 import rode from "../image/rode.png"
 import shopify from "../image/shopify.png"
 import paytm from "../image/paytm.png"
-import ATG from "../image/ATG.png"
+import shoping from "../image/productscreen.png"
 import ecommerce from "../image/e_commerce.png"
 import e_commerce from "../image/ecommerce.png"
 
 export const PortfolioData = [
     {
-        id:1,
+        id:0,
         img: rode,
         link: "https://velvety-genie-9718a9.netlify.app/",
         link2: "https://github.com/Ankush8950/rode-clone"
     },  
     {
-        id:2,
-        img: ATG,
-        link:"https://xenodochial-blackwell-069db7.netlify.app/",
-        link2: "https://github.com/Ankush8950/ATG.WORLDsign_signup"
+        id:1,
+        img: shoping,
+        link: "https://coruscating-biscotti-cd60f8.netlify.app/",
+        link2: "https://github.com/Ankush8950/E-Commerce-Application-Clone"
     },
     {
-        id:3,
+        id:2,
         img: e_commerce,
         link: "",
         link2: "https://github.com/Ankush8950/mobile-shoping-app"
 
     },
     {
-        id:4,
+        id:3,
         img: ecommerce,
         link: "https://vigilant-shaw-9425f4.netlify.app",
         link2: "https://github.com/Ankush8950/e-commerce"
     },
     {
-        id:5,
+        id:4,
         img: shopify,
         link: "https://statuesque-centaur-5ea6ce.netlify.app/",
         link2: "https://github.com/Ankush8950/shopify-Clone"
     },
     {
-        id:6,
+        id:5,
         img: paytm,
         link: "https://exquisite-mermaid-d537e6.netlify.app/",
         link2: "https://github.com/Ankush8950/PaytmClone"
